e74de5c7bee4549ee064297c3982d2679df9aa3a
quangkhanh27 <lamquangkhanh2710@gmail.com>
2021-05-18 16:30:12 +0700

    push code

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from PIL import Image
+import cv2
+import numpy as np
+from flask_cors import CORS
+from flask import Flask, request, json
+import base64
 
 app = Flask(__name__)
 
-@app.route("/")
+
+def base64_to_array(base64_string):
+    nparr = np.fromstring(base64.b64decode(base64_string), np.uint8)
+    return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+
+@app.route("/",  methods=['POST'])
 def hello():
-    return "Lâm Quang Khánh"
+    image = base64_to_array(request.form['File'])
+    cv2.imwrite("hinh.png", image)
+    return "image"
+
+
+@app.route("/",  methods=['GET'])
+def hello1():
+    return "oke"
+
 
 if __name__ == "__main__":
     app.run()
+CORS(app)
